26b1f21e80cfb7caedbc40a0311c8878ad2f521b
Jens <jens.vdwiel@gmail.com>
2024-07-15 22:35:00 +0100

    Added test for JSON serializer

--- valkeystore_test.go
+++ valkeystore_test.go
@@ -217,3 +217,49 @@ func TestDelete(t *testing.T) {
 		t.Fatal("failed to delete session: ", err)
 	}
 }
+
+func TestJSONSerializer(t *testing.T) {
+	ctx := context.Background()
+	vk, err := setupValkey(ctx)
+	if err != nil {
+		t.Fatal("failed to setup valkey container", err)
+	}
+
+	defer func() {
+		if err := vk.Terminate(ctx); err != nil {
+			t.Fatal("failed to terminate container", err)
+		}
+	}()
+
+	endpoint, err := vk.MappedPort(ctx, "6379/tcp")
+	if err != nil {
+		return
+	}
+
+	client, err := valkey.NewClient(valkey.ClientOption{
+		InitAddress: []string{"localhost:" + endpoint.Port()},
+	})
+	if err != nil {
+		t.Fatal("failed to create valkey client", err)
+	}
+
+	defer client.Close()
+
+	store, err := NewValkeyStore(client, WithSerializer(NewJSONSerializer()))
+	if err != nil {
+		t.Fatal("failed to create redis store", err)
+	}
+
+	req, err := http.NewRequest("GET", "http://www.example.com", nil)
+	if err != nil {
+		t.Fatal("failed to create request", err)
+	}
+
+	session, err := store.New(req, "hello")
+	if err != nil {
+		t.Fatal("failed to create session", err)
+	}
+	if session.IsNew == false {
+		t.Fatal("session is not new")
+	}
+}
